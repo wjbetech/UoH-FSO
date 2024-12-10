12ae1d3ab4560a1bcd763f799ed3d1a63a1ac2d8
wjbetech <wjbetech@gmail.com>
2024-12-10 09:30:26 +0900

    Updated 1.2 to 1.3 spec

--- Part 1/courseinfo/1.2/src/App.jsx
+++ Part 1/courseinfo/1.2/src/App.jsx
@@ -1,21 +1,42 @@
-import { Content } from "./components/Content";
+// We can bypass the <Content> element now since parts are separate objects
+// import { Content } from "./components/Content";
+
+// Instead just import the <Part> element - it matches the data anyways
+import { Part } from "./components/Part";
 import { Header } from "./components/Header";
 import { Total } from "./components/Total";
 
 const App = () => {
-  // cleaned up the data into an array because feng shui
-  const parts = [
-    { part: "Fundamentals of React", exercises: 10 },
-    { part: "Using props to pass data", exercises: 7 },
-    { part: "State of a component", exercises: 14 }
-  ];
+  const course = "Half Stack application development";
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14
+  };
 
   return (
     <div>
-      <Header content="Half Stack application development" />
-      <Content parts={parts} />
-      {/* this part could be handled cleaner outside of the return-render */}
-      <Total total={`${parts[0].exercises + parts[1].exercises + parts[2].exercises}`} />
+      <Header content={course} />
+      <Part
+        part={part1.name}
+        exercises={part1.exercises}
+      />
+      <Part
+        part={part2.name}
+        exercises={part2.exercises}
+      />
+      <Part
+        part={part3.name}
+        exercises={part3.exercises}
+      />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   );
 };
--- Part 1/courseinfo/1.2/src/components/Content.jsx
+++ Part 1/courseinfo/1.2/src/components/Content.jsx
@@ -1,20 +1,22 @@
-import { Part } from "./Part";
+// Commenting out <Content> because it's currently not being used
 
-export const Content = ({ parts }) => {
-  return (
-    <div>
-      <Part
-        part={parts[0].part}
-        exercises={parts[0].exercises}
-      />
-      <Part
-        part={parts[1].part}
-        exercises={parts[1].exercises}
-      />
-      <Part
-        part={parts[2].part}
-        exercises={parts[2].exercises}
-      />
-    </div>
-  );
-};
+// import { Part } from "./Part";
+//
+// export const Content = ({ parts }) => {
+//   return (
+//     <div>
+//       <Part
+//         part={parts[0].part}
+//         exercises={parts[0].exercises}
+//       />
+//       <Part
+//         part={parts[1].part}
+//         exercises={parts[1].exercises}
+//       />
+//       <Part
+//         part={parts[2].part}
+//         exercises={parts[2].exercises}
+//       />
+//     </div>
+//   );
+// };
