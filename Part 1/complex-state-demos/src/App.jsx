0fac2636a2fb31a85c974989ed53aa5d1ba4813b
wjbetech <wjbetech@gmail.com>
2024-12-11 14:41:24 +0900

    Adding directory for demoing complex state

---
+++ Part 1/complex-state-demos/src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from "react";
+
+const App = () => {
+  return (
+    <div>
+      {/* <span>{clicks.left}</span>
+      <button onClick={handleLeftClick}>Left</button>
+      <button onClick={handleRightClick}>Right</button>
+      <span>{clicks.right}</span> */}
+    </div>
+  );
+};
+
+export default App;
