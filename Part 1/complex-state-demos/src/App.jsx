659d8599cbc83cb13079d3eb160545dd75de9cdd
wjbetech <wjbetech@gmail.com>
2024-12-11 15:13:36 +0900

    Pushing the demo counter code

--- Part 1/complex-state-demos/src/App.jsx
+++ Part 1/complex-state-demos/src/App.jsx
@@ -1,12 +1,20 @@
-// import { useState } from "react";
+import { useState } from "react";
 
 const App = () => {
+  const [counts, setCounts] = useState({
+    left: 0,
+    right: 0
+  });
+
+  const handleLeftClick = () => setCounts({ ...counts, left: counts.left + 1 });
+  const handleRightClick = () => setCounts({ ...counts, right: counts.right + 1 });
+
   return (
     <div>
-      {/* <span>{clicks.left}</span>
-      <button onClick={handleLeftClick}>Left</button>
-      <button onClick={handleRightClick}>Right</button>
-      <span>{clicks.right}</span> */}
+      <span>{counts.left}</span>
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      <span>{counts.right}</span>
     </div>
   );
 };
