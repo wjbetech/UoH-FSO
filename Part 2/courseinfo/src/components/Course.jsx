87bf4ef5f6e08a81622c30c8345089da48490ee8
wjbetech <wjbetech@gmail.com>
2024-12-16 21:28:51 +0900

    Working part2-2

--- Part 2/courseinfo/src/components/Course.jsx
+++ Part 2/courseinfo/src/components/Course.jsx
@@ -1,12 +1,18 @@
 import Header from "./Header";
 import Content from "./Content";
+import Total from "./Total";
 
 export default function Course({ course }) {
   const { name, parts } = course;
+
+  const totalExercises = parts.reduce((sum, p) => sum + p.exercises, 0);
+  console.log(totalExercises);
+
   return (
     <div>
       <Header name={name} />
       <Content parts={parts} />
+      <Total total={totalExercises} />
     </div>
   );
 }
---
+++ Part 2/courseinfo/src/components/Total.jsx
@@ -0,0 +1,3 @@
+export default function Total({ total }) {
+  return <span style={{ fontWeight: "bold" }}>Total Exercises: {total}</span>;
+}
