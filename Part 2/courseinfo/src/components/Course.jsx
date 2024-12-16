e5dd55978fc022e588514056ac38a0de315e2b1a
wjbetech <wjbetech@gmail.com>
2024-12-16 21:30:52 +0900

    Pushing part2-3

--- Part 2/courseinfo/src/components/Course.jsx
+++ Part 2/courseinfo/src/components/Course.jsx
@@ -5,6 +5,7 @@ import Total from "./Total";
 export default function Course({ course }) {
   const { name, parts } = course;
 
+  // I already did this, just pushing a part2-3 anyways for completeness
   const totalExercises = parts.reduce((sum, p) => sum + p.exercises, 0);
   console.log(totalExercises);
 
