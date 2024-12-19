aa1d1cbd7b05bae033b36636cdd9e8190b7558d2
wjbetech <wjbetech@gmail.com>
2024-12-19 13:22:00 +0900

    Setting up part2-10 directory

--- Part 1/event-handling-revisited/src/components/Button.jsx
+++
@@ -1,6 +0,0 @@
-// changed handleClick prop to onClick, doesn't cause any conflicts
-// but this may not be a very good practice
-
-const Button = ({ onClick, label }) => <button onClick={onClick}>{label}</button>;
-
-export default Button;
