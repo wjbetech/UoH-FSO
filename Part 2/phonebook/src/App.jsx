788cc0a3035cf5ba59315c6df48463b8b8e33448
wjbetech <wjbetech@gmail.com>
2024-12-19 13:35:22 +0900

    Finished part2-6

--- Part 2/phonebook/src/App.jsx
+++ Part 2/phonebook/src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+
+  // only used to control the form input element
+  const [newName, setNewName] = useState("");
+
+  const handleChange = (event) => {
+    console.log(newName);
+    setNewName(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setPersons([...persons, { name: newName }]);
+    setNewName("");
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name:{" "}
+          <input
+            value={newName}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <div key={person.name}>
+          <p>{person.name}</p>
+        </div>
+      ))}
+      {/* <div style={{ marginTop: "64px" }}>
+        <h5>Debugger:</h5>
+        <p>{newName}</p>
+      </div> */}
+    </div>
+  );
+};
 
-export default App
+export default App;
