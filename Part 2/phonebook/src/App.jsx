f3193bfa38ffc42e358374b10ab71697590a10f6
wjbetech <wjbetech@gmail.com>
2024-12-24 02:18:13 +0900

    Completed up to step 10, 2.15

--- Part 2/phonebook/src/App.jsx
+++ Part 2/phonebook/src/App.jsx
@@ -1,52 +1,115 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Person from "./components/Person";
+import personService from "./services/persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
-
-  // only used to control the form input element
-  const [newName, setNewName] = useState("");
+  const [persons, setPersons] = useState([]);
+  const [newPerson, setNewPerson] = useState({
+    name: "",
+    number: ""
+  });
 
   const handleChange = (event) => {
-    setNewName(event.target.value);
+    const { name, value } = event.target;
+    setNewPerson({
+      ...newPerson,
+      [name]: value
+    });
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  useEffect(() => {
+    const fetchPersons = async () => {
+      const persons = await personService.getAllPeople();
+      setPersons(persons);
+    };
+    fetchPersons();
+  }, []);
 
-    // build our new name object
-    const newNameObject = {
-      name: newName
+  const addPerson = async (event) => {
+    event.preventDefault();
+    const newPersonObject = {
+      name: newPerson.name,
+      number: newPerson.number,
+      // making these strings just because the code provided to start
+      // this task was using strings for ids
+      // -- this is actually a pretty bad solution for creating ids though
+      // because if you are deleting/creating entries you will quite quickly
+      // trip over items trying to have the same key
+      // -- easily fixed with uuid or even simple Math.random
+      id: new String(persons.length + 1)
     };
 
-    // METHOD 1 - return after finding duplicate name
-    if (persons.find((p) => p.name === newNameObject.name)) {
-      alert(`${newNameObject.name} is already in the phonebook!`);
-      return;
+    // see if name already exists in phonebook
+    if (persons.find((person) => person.name === newPersonObject.name)) {
+      // ask user if they want to replace the number with the new number provided
+      if (
+        !window.confirm(
+          `${newPersonObject.name} is already added to the phonebook, replace the old number with a new one?`
+        )
+      ) {
+        return;
+      } else {
+        // find the existing object with the same name
+        const personToUpdate = persons.find((person) => person.name === newPersonObject.name);
+        // create the object by spreading the old object but update the number
+        const updatedPerson = { ...personToUpdate, number: newPersonObject.number };
+        // pass the ID and new person object to the updatePerson service
+        personService.updatePerson(personToUpdate.id, updatedPerson);
+        // setPersons iterates over persons again and replaces the old person with new
+        // and leaves the rest as they were, "person"
+        setPersons(persons.map((person) => (person.id === personToUpdate.id ? updatedPerson : person)));
+        return;
+      }
     }
-    setPersons(persons.concat(newNameObject));
-    setNewName("");
 
-    // METHOD 2 - if/else
-    // if (persons.find((p) => p.name === newNameObject.name)) {
-    //   alert(`${newNameObject.name} is already in the phonebook!`);
-    //   return;
-    // } else {
-    //   setPersons([...persons, newNameObject]);
-    //   setNewName("");
-    // }
+    const newPersonResponse = await personService.addPerson(newPersonObject);
+    setPersons(persons.concat(newPersonResponse));
+    setNewPerson({ name: "", number: "" });
+  };
 
-    // NB Can also use .some() or .filter() to get similar/same functionality
-    // but remember to opt for performative methods!
+  const handleDeletePerson = async (id) => {
+    try {
+      // get the person to delete
+      const personToDelete = persons.find((person) => person.id === id);
+
+      // comfy c.log to see what we are deleting
+      console.log(`Attempting to delete ${personToDelete.name}`);
+
+      // confirm deleting, return out of handleDelete on cancel
+      if (!window.confirm(`Delete ${personToDelete.name}?`)) {
+        return;
+      }
+
+      // try to delete on backend
+      await personService.deletePerson(id);
+
+      // remove person from state - triggering re-render
+      setPersons(persons.filter((person) => person.id !== id));
+
+      // log success
+      console.log(`${personToDelete.name} deleted successfully!`);
+    } catch (error) {
+      alert("Could not delete that person! Check code and try again.");
+      console.log("Could not delete a person: ");
+      console.log(error);
+    }
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          name:{" "}
+      <form onSubmit={addPerson}>
+        <div style={{ display: "flex", flex: "flex-row", gap: "8px" }}>
+          <label htmlFor="name">Name: </label>
           <input
-            value={newName}
+            name="name"
+            value={newPerson.name}
+            onChange={handleChange}
+          />
+          <label htmlFor="number">Number: </label>
+          <input
+            name="number"
+            value={newPerson.number}
             onChange={handleChange}
           />
         </div>
@@ -55,15 +118,13 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person, i) => (
-        <div key={i}>
-          <p>{person.name}</p>
-        </div>
+      {persons.map((person) => (
+        <Person
+          key={person.id}
+          person={person}
+          handleDelete={handleDeletePerson}
+        />
       ))}
-      {/* <div style={{ marginTop: "64px" }}>
-        <h5>Debugger:</h5>
-        <p>{newName}</p>
-      </div> */}
     </div>
   );
 };
---
+++ Part 2/phonebook/src/components/Person.jsx
@@ -0,0 +1,9 @@
+const Person = ({ person, handleDelete }) => {
+  return (
+    <li>
+      {person.name} - {person.number} <button onClick={() => handleDelete(person.id)}>Delete</button>
+    </li>
+  );
+};
+
+export default Person;
