1d49fbda1e9373e59af1c939fb73aec7553175c3
wjbetech <wjbetech@gmail.com>
2025-01-01 17:48:33 +0900

    I think I have finished Part 2.20

--- Part 2/countries/src/components/Countries.jsx
+++ Part 2/countries/src/components/Countries.jsx
@@ -1,9 +1,33 @@
+import Country from "./Country";
+import { useState } from "react";
+
 export default function Countries({ filteredCountries }) {
+  const [viewCountry, setViewCountry] = useState(false);
+
+  const handleToggleCountry = (cioc) => {
+    setViewCountry((prev) => (prev === cioc ? null : cioc));
+  };
+
   console.log("Returning countries: ", filteredCountries);
   return (
     <div className="country-list">
       {filteredCountries.map((country) => (
-        <p key={country.cca2}>{country.name.common}</p>
+        <div
+          key={country.cioc}
+          className="country"
+        >
+          <div className="single-country">
+            <p>{country.name.common}</p>
+            <button onClick={() => handleToggleCountry(country.cioc)}>
+              {viewCountry === country.cioc ? "Hide" : "Show"}
+            </button>
+          </div>
+          {viewCountry === country.cioc && (
+            <div className="country-view">
+              <Country countryDetails={country} />
+            </div>
+          )}
+        </div>
       ))}
     </div>
   );
--- Part 2/countries/src/components/Country.jsx
+++ Part 2/countries/src/components/Country.jsx
@@ -1,9 +1,31 @@
+import { useState, useEffect } from "react";
+import getCityWeather from "../services/fetchWeather";
+
 export default function Country({ countryDetails }) {
+  const [weather, setWeather] = useState(null);
+
   console.log("Returning single country: ", countryDetails);
   const { name, capital, area, languages, flags } = countryDetails;
 
   const countryLanguages = Object.values(languages);
-  console.log(countryLanguages);
+  console.log(`Languages of ${name.common}: `, countryLanguages);
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      const weather = await getCityWeather(capital);
+      setWeather(weather);
+      console.log("Weather in capital: ", weather);
+    };
+    fetchWeather();
+
+    // Clean up function
+    return () => {
+      // Cancel any pending requests
+      console.log("Cleanup function called");
+    };
+  }, [capital]);
+
+  const weatherIconURL = weather && `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`;
 
   return (
     <div className="country-details">
@@ -24,6 +46,17 @@ export default function Country({ countryDetails }) {
         src={flags.png}
         alt=""
       />
+      {weather && (
+        <div className="country-weather">
+          <h3>Weather in {capital}</h3>
+          <p>Temperature: {weather.main.temp.toFixed(0)}°C</p>
+          <img
+            src={weatherIconURL}
+            alt=""
+          />
+          <p>Wind Speed: {weather.wind.speed} m/s</p>
+        </div>
+      )}
     </div>
   );
 }
