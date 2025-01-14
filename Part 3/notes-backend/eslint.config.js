6a573f909ff5a8cc13c93f870a693da892d03c9a
wjbetech <wjbetech@github.com>
2025-01-14 15:11:05 +0900

    pushing finished part 3 demoing pre-linting

---
+++ Part 3/notes-backend/eslint.config.js
@@ -0,0 +1,46 @@
+import globals from "globals";
+import js from "@eslint/js"
+import stylisticJs from "@stylistic/eslint-plugin-js"
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "module",
+      globals: {
+        ...globals.node
+      },
+      ecmaVersion: "latest"
+    },
+    plugins: {
+      "@stylistic/js": stylisticJs
+    },
+    rules: {
+      "eqeqeq": "error",
+      "no-trailing-spaces": "error",
+      "object-curly-spacing": [
+        "error", "always"
+      ],
+      "arrow-spacing": [
+        "error", { "before": true, "after": true },
+      ],
+      "no-console": "off",
+      "@stylistic/js/indent": [
+        "error",
+        2
+      ],
+      "@stylistic/js/linebreak-style": [
+        "error",
+        "unix"
+      ],
+      "@stylistic/js/quotes": [
+        "error",
+        "double"
+      ],
+    },
+    ignores: ["dist/**", "build/**"]
+  }
+];
