5bbf3778d91f62f4064f324d1fe5c5557d34fe07
wjbetech <wjbetech@gmail.com>
2025-01-18 16:22:41 +0900

    Added lodash, pushing part 4.6 spec blog

--- Part 4/blog/blog-backend/tests/blogDummy.test.js
+++ Part 4/blog/blog-backend/tests/blogDummy.test.js
@@ -1,8 +1,9 @@
 import { test, describe } from "node:test";
 import assert from "node:assert";
 import tests from "./test.js";
+import dummyData from "./dummyData.json" assert { type: "json" };
 
-const { blogDummy, totalLikes } = tests;
+const { blogDummy, totalLikes, favoriteBlog, mostBlogs } = tests;
 
 describe("Blog", () => {
   test("returns a value of 1", () => {
@@ -11,7 +12,7 @@ describe("Blog", () => {
   });
 });
 
-describe("Blog total likes when", () => {
+describe("Blog total likes", () => {
   const oneBlog = [
     {
       title: "Blog post one",
@@ -42,11 +43,49 @@ describe("Blog total likes when", () => {
     }
   ];
 
-  test("list has one blog", () => {
+  test("when blog list has one blog", () => {
     assert.strictEqual(totalLikes(oneBlog), 8);
   });
 
-  test("list has multiple blogs", () => {
+  test("when blog list has multiple blogs", () => {
     assert.strictEqual(totalLikes(multiBlog), 25);
+  });
+});
+
+describe("Look for: ", () => {
+  const multiBlog = [
+    {
+      title: "Blog post one",
+      author: "John Doe",
+      url: "https://www.example.com",
+      likes: 8
+    },
+    {
+      title: "Blog post two",
+      author: "Jane Doe",
+      url: "https://www.example.net",
+      likes: 5
+    },
+    {
+      title: "Blog post three",
+      author: "Bob Smith",
+      url: "https://www.example.org",
+      likes: 12
+    }
+  ];
+
+  test("most popular blog", () => {
+    assert.deepStrictEqual(favoriteBlog(multiBlog), {
+      title: "Blog post three",
+      author: "Bob Smith",
+      likes: 12
+    });
+  });
+
+  test("most prolific author", () => {
+    assert.deepStrictEqual(mostBlogs(dummyData), {
+      author: "Robert C. Martin",
+      blogs: 3
+    });
   });
 });
--- Part 4/blog/blog-backend/tests/test.js
+++ Part 4/blog/blog-backend/tests/test.js
@@ -1,3 +1,5 @@
+import lodash from "lodash";
+
 const reverseString = (string) => {
   return string.split("").reverse().join("");
 };
@@ -18,4 +20,20 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
-export default { reverseString, arrAverage, blogDummy, totalLikes };
+const favoriteBlog = (blogs) => {
+  const favBlog = blogs.reduce((fav, current) => {
+    return current.likes > fav.likes ? current : fav;
+  });
+  return { title: favBlog.title, author: favBlog.author, likes: favBlog.likes };
+};
+
+const mostBlogs = (blogs) => {
+  const groupedByAuthor = lodash.groupBy(blogs, "author");
+  const mostBlogsAuthor = lodash.maxBy(Object.keys(groupedByAuthor), (author) => groupedByAuthor[author].length);
+  return {
+    author: mostBlogsAuthor,
+    blogs: groupedByAuthor[mostBlogsAuthor].length
+  };
+};
+
+export default { reverseString, arrAverage, blogDummy, totalLikes, favoriteBlog, mostBlogs };
