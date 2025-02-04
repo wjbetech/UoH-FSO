545380e8a4c63ee07027d0d91355aea2fa851cdf
wjbetech <wjbetech@gmail.com>
2025-02-04 13:28:27 +0900

    Pushing working code for Part 5, ex 5.7

--- Part 5/blog/blog-frontend/src/components/Blog.jsx
+++ Part 5/blog/blog-frontend/src/components/Blog.jsx
@@ -1,22 +1,46 @@
+import { useState } from "react";
+
 export default function Blog({ blogInfo, handleDelete }) {
+  const [showDetails, setShowDetails] = useState(false);
   const { title, content, author, likes } = blogInfo;
+
+  const toggleDetails = () => setShowDetails(!showDetails);
 
   return (
     <div className="blog-post">
-      <h2 className="blog-title">{title}</h2>
-      <div className="blog-post-buttons">
-        <button className="view-button">View</button>
-        <button className="like-button">Like</button>
-        <button
-          className="delete-button"
-          onClick={handleDelete}
-        >
-          Delete
-        </button>
-      </div>
-      <p className="blog-author">Author: {author}</p>
-      <p className="blog-content">{content}</p>
-      <p>Likes: {likes}</p>
+      {showDetails ? (
+        <div className="full-blog-post">
+          <h2 className="blog-title">{title}</h2>
+          <div className="blog-post-buttons">
+            <button
+              onClick={toggleDetails}
+              className="view-button"
+            >
+              Close
+            </button>
+            <button className="like-button">Like</button>
+            <button
+              className="delete-button"
+              onClick={handleDelete}
+            >
+              Delete
+            </button>
+          </div>
+          <p className="blog-author">Author: {author}</p>
+          <p className="blog-content">{content}</p>
+          <p>Likes: {likes}</p>
+        </div>
+      ) : (
+        <div className="mini-blog-post">
+          <h2 className="blog-title">{title}</h2>
+          <button
+            onClick={toggleDetails}
+            className="view-button"
+          >
+            View
+          </button>
+        </div>
+      )}
     </div>
   );
 }
