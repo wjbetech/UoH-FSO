b4d63f528981a06af7c21d712bcee2e593eb784c
wjbetech <wjbetech@gmail.com>
2025-03-03 00:26:12 +0900

    Pushing code up to Part 6, ex. 6.6

--- Part 6/redux-anecdotes/src/App.jsx
+++ Part 6/redux-anecdotes/src/App.jsx
@@ -1,34 +1,50 @@
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from "react-redux";
+import { voteAnecdote, createAnecdote } from "./reducers/anecdoteReducer.js";
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const anecdotes = useSelector((state) => state);
 
-  const vote = (id) => {
-    console.log('vote', id)
-  }
+  // you need to pass the payload as id
+  // don't overthink the term "payload", you pass whatever data is needed
+  // for the action to be executed, in our case we just need the id
+  const addVote = (id) => {
+    dispatch(voteAnecdote(id));
+  };
+
+  // I think there may be a better way to handle this
+  // perhaps by destructuring the '.anecdote.value' value
+  // but for now I think this works
+  const addAnecdote = (event) => {
+    event.preventDefault();
+    const content = event.target.anecdote.value;
+    event.target.anecdote.value = "";
+    dispatch(createAnecdote(content));
+  };
+
+  anecdotes.sort((a, b) => b.votes - a.votes);
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+      {anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
+          <div>{anecdote.content}</div>
+          <div>Likes: {anecdote.votes}</div>
+          <button onClick={() => addVote(anecdote.id)}>vote</button>
+          <br />
+          <br />
         </div>
-      )}
-      <h2>create new</h2>
-      <form>
-        <div><input /></div>
-        <button>create</button>
+      ))}
+      <h2>Add Anecdote</h2>
+      <form onSubmit={addAnecdote}>
+        <div>
+          <input name="anecdote" />
+        </div>
+        <button type="submit">Add</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- Part 6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ Part 6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,29 +1,49 @@
 const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
+  "If it hurts, do it more often",
+  "Adding manpower to a late software project makes it later!",
+  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+  "Premature optimization is the root of all evil.",
+  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it."
+];
 
-const getId = () => (100000 * Math.random()).toFixed(0)
+const getId = () => (100000 * Math.random()).toFixed(0);
 
 const asObject = (anecdote) => {
   return {
     content: anecdote,
     id: getId(),
     votes: 0
-  }
-}
+  };
+};
 
-const initialState = anecdotesAtStart.map(asObject)
+const initialState = anecdotesAtStart.map(asObject);
+
+export const voteAnecdote = (id) => {
+  return {
+    type: "anecdotes/vote",
+    payload: { id }
+  };
+};
+
+export const createAnecdote = (content) => {
+  return {
+    type: "anecdotes/addAnecdote",
+    payload: content
+  };
+};
 
 const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
+  switch (action.type) {
+    case "anecdotes/vote":
+      return state.map((anecdote) =>
+        anecdote.id === action.payload.id ? { ...anecdote, votes: anecdote.votes + 1 } : anecdote
+      );
+    case "anecdotes/addAnecdote":
+      return [...state, asObject(action.payload)];
+    default:
+      return state;
+  }
+};
 
-  return state
-}
-
-export default reducer
+export default reducer;
