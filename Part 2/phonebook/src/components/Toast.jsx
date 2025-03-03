e63df326931dd67ed7d7cd6f1ec1f0f6be8bd3c1
wjbetech <wjbetech@gmail.com>
2025-03-03 11:21:43 +0900

    Pushing clean Part 6, ex 6.8 finished code

--- Part 2/phonebook/src/components/Toast.jsx
+++
@@ -1,4 +0,0 @@
-export default function Toast({ message, status }) {
-  const toastStatus = status === "error" ? "toast-error" : "toast-success";
-  return <div className={`${toastStatus}`}>{message}</div>;
-}
