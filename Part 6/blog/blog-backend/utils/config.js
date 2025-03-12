c923caf090b4ca8d65c847bb3c540aaa3189a8c8
wjbetech <wjbetech@gmail.com>
2025-03-12 20:05:35 +0900

    Pushing working clean repo for Part 6, ex. 6.23 and 6.24

--- Part 6/blog/blog-backend/utils/config.js
+++
@@ -1,6 +0,0 @@
-import "dotenv/config";
-
-const PORT = process.env.PORT;
-const MONGODB_URI = process.env.NODE_ENV === "test" ? process.env.MONGODB_TEST_URI : process.env.MONGODB_BLOG_URI;
-
-export default { PORT, MONGODB_URI };
