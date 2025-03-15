d3506f6c00b970d77475d32bb76b8586e28ab444
wjbetech <wjbetech@gmail.com>
2025-03-15 23:18:17 +0900

    Pushing clean working exercises up to Part 7, ex 7.8

--- Part 2/phonebook/src/utils/textSanitizier.js
+++
@@ -1,7 +0,0 @@
-const normalizeName = (name) => {
-  const splitName = name.split(" ");
-  const capitalizedName = splitName.map((part) => part.charAt(0).toUpperCase() + part.slice(1).toLowerCase());
-  return capitalizedName.join(" ");
-};
-
-export default { normalizeName };
--- Part 5/blog/blog-backend/utils/logger.js
+++
@@ -1,9 +0,0 @@
-const info = (...params) => {
-  console.log(...params);
-};
-
-const error = (...params) => {
-  console.log(...params);
-};
-
-export default { info, error };
