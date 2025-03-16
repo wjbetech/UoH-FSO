5d2816c3934b4e7842dff805fe403afd664ad963
wjbetech <wjbetech@gmail.com>
2025-03-16 22:56:22 +0900

    Pushing webpack demo project up to Loaders section

---
+++ Part 7/webpack-demo/src/App.js
@@ -0,0 +1,12 @@
+import React from "react"; // we need this now also in component files
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <p>This is a simple React application, bundled with Webpack!</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ Part 7/webpack-demo/src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react/client";
+import App from "./App.js";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
