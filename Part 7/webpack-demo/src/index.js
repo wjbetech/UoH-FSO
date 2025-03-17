5eeb3fd6005f7e41621d22e0e12e2191acdf5fcf
wjbetech <wjbetech@gmail.com>
2025-03-17 22:44:56 +0900

    Pushing demo code up to Part 7, section f

--- Part 7/webpack-demo/src/index.js
+++ Part 7/webpack-demo/src/index.js
@@ -1,5 +1,9 @@
+import "core-js/stable/index.js";
+import "regenerator-runtime/runtime.js";
+
 import React from "react";
-import ReactDOM from "react/client";
+import ReactDOM from "react-dom/client";
 import App from "./App.js";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
