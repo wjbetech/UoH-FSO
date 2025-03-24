03ce74932984fd9ae1c3f9588d773fd234605d45
wjbetech <wjbetech@gmail.com>
2025-03-25 08:37:19 +0900

    Pushing some FE code for comments

---
+++ Part 7/blog/blog-frontend/src/services/comments.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+const baseUrl = "/api/blogs";
+
+let token = null;
+
+const setToken = (newToken) => {
+  token = `Bearer ${newToken}`;
+};
+
+const create = async (blogId, commentData) => {
+  const config = {
+    headers: { Authorization: token },
+  };
+
+  const res = await axios.post(`${blogId}/comments`, commentData, config);
+
+  return res.data;
+};
+
+const remove = async (commentId) => {
+  const config = {
+    headers: { Authorization: token },
+  };
+
+  const res = await axios.delete(`/api/comments/${commentId}`, config);
+
+  return res.data;
+};
+
+export default { create, remove };
