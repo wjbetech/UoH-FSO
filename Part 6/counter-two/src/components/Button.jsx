caf7f96aeed5a0581740a042e206c2885648be8d
wjbetech <wjbetech@gmail.com>
2025-03-31 21:40:16 +0900

    Pushing working clean solutions (I hope) up to Part 8, ex. 8.16

--- Part 6/counter-two/src/components/Button.jsx
+++
@@ -1,8 +0,0 @@
-import { useCounterDispatch } from "../CounterContext";
-
-const Button = ({ type, label }) => {
-  const dispatch = useCounterDispatch();
-  return <button onClick={() => dispatch({ type })}>{label}</button>;
-};
-
-export default Button;
