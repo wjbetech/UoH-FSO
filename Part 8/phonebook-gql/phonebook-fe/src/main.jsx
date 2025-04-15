bd55715fb7c6c10c4f2b8323cbb4e1d9239bda82
wjbetech <wjbetech@github.com>
2025-04-15 19:42:13 +0900

    pushing some sub to client code

--- Part 8/phonebook-gql/phonebook-fe/src/main.jsx
+++ Part 8/phonebook-gql/phonebook-fe/src/main.jsx
@@ -1,8 +1,13 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-import { ApolloClient, createHttpLink, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { ApolloClient, createHttpLink, ApolloProvider, InMemoryCache, split } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
+
+// access subscriptions on the FE React client
+import { getMainDefinition } from "@apollo/client/utilities";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions"
+import { createClient } from "graphql-ws"
 
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem("phonebook-user-token");
@@ -18,9 +23,26 @@ const httpLink = createHttpLink({
   uri: "http://localhost:4000"
 });
 
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: "ws://localhost:4000"
+  })
+)
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query)
+    return (
+      definition.kind === "OperationDefinition" && definition.operation === "subscription"
+    )
+  },
+  wsLink,
+  authLink.concat(httpLink)
+)
+
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: authLink.concat(httpLink),
+  link: splitLink,
   uri: "http://localhost:4000/graphql",
   headers: {
     authorization: localStorage.getItem("phonebook-user-token")
