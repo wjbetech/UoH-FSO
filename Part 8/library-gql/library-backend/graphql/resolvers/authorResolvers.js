73ef49e2b93549e44a791b171beb11a5850a080c
wjbetech <wjbetech@gmail.com>
2025-04-17 21:06:51 +0900

    Pushing (I hope) working code for Part 8, ex. 8.26

--- Part 8/library-gql/library-backend/graphql/resolvers/authorResolvers.js
+++ Part 8/library-gql/library-backend/graphql/resolvers/authorResolvers.js
@@ -5,22 +5,38 @@ import { GraphQLError } from "graphql";
 const authorResolver = {
   Query: {
     authorCount: async () => await Author.countDocuments(),
-    allAuthors: async () => await Author.find({})
-  },
-  Author: {
-    authorBookCount: async (root) => {
-      const bookCount = await Book.countDocuments({ author: root._id });
-      return bookCount;
+    allAuthors: async () => {
+      return Author.aggregate([
+        {
+          $lookup: {
+            from: "books",
+            localField: "_id",
+            foreignField: "author",
+            as: "books"
+          }
+        },
+        {
+          $addFields: {
+            authorBookCount: { $size: "$books" }
+          }
+        },
+        {
+          $project: {
+            _id: 0,
+            id: "$_id",
+            name: 1,
+            born: 1,
+            authorBookCount: 1
+          }
+        }
+      ]);
     }
   },
   Mutation: {
     updateAuthorBorn: async (root, args, { currentUser }) => {
-      // ensure only a logged in user can update an author's born value
       if (!currentUser) {
         throw new GraphQLError("addPerson error - not authenticated!", {
-          extensions: {
-            code: "BAD_USER_INPUT"
-          }
+          extensions: { code: "BAD_USER_INPUT" }
         });
       }
 
@@ -32,24 +48,7 @@ const authorResolver = {
 
       if (!updateAuthor) {
         throw new GraphQLError("Author not found", {
-          extensions: {
-            code: "BAD_USER_INPUT",
-            invalidArgs: args.name
-          }
-        });
-      }
-
-      updateAuthor.born = args.born;
-
-      try {
-        await updateAuthor.save();
-      } catch (error) {
-        throw new GraphQLError("mutation updateAuthorBorn failed: ", {
-          extensions: {
-            code: "BAD_USER_INPUT",
-            invalidArgs: args.name,
-            error
-          }
+          extensions: { code: "BAD_USER_INPUT", invalidArgs: args.name }
         });
       }
 
