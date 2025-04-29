09a7a2bf28c78b034ea03b6efcc900f5d9883c28
wjbetech <wjbetech@github.com>
2025-04-29 09:34:31 +0900

    Pushing some Zod demo

--- Part 9/ilari-diary/src/utils/utils.ts
+++ Part 9/ilari-diary/src/utils/utils.ts
@@ -3,23 +3,15 @@ import { Weather, Visibility } from "../types/types";
 import { z } from "zod";
 // removed imports: parseComment, parseDate, parseWeather, parseVisibility
 
-const toNewFlightLogEntry = (object: unknown): NewFlightLogEntry => {
-  if (!object || typeof object !== "object") {
-    throw new Error("Incorrect or missing data: " + object);
-  }
+const newEntrySchema = z.object({
+  weather: z.nativeEnum(Weather),
+  visibility: z.nativeEnum(Visibility),
+  date: z.string().date(),
+  comment: z.string().optional()
+});
 
-  if ("comment" in object && "date" in object && "weather" in object && "visibility" in object) {
-    const newFlightLog: NewFlightLogEntry = {
-      weather: z.nativeEnum(Weather).parse(object.weather),
-      visibility: z.nativeEnum(Visibility).parse(object.visibility),
-      date: z.string().date().parse(object.date),
-      comment: z.string().parse(object.comment)
-    };
-
-    return newFlightLog;
-  }
-
-  throw new Error("Incorrect data: some fields are missing");
+export const toNewFlightLogEntry = (object: unknown): NewFlightLogEntry => {
+  return newEntrySchema.parse(object);
 };
 
 export default toNewFlightLogEntry;
