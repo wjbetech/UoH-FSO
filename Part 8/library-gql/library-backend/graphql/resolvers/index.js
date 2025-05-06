fc44ae1b773b0b729e99f6fe4bcc52d22157fbdb
wjbetech <wjbetech@gmail.com>
2025-05-06 16:09:40 +0900

    Pushing clean Part 9 for ex. 9.22

--- Part 8/library-gql/library-backend/graphql/resolvers/index.js
+++
@@ -1,7 +0,0 @@
-import { mergeResolvers } from "@graphql-tools/merge";
-import authorResolver from "./authorResolvers.js";
-import bookResolver from "./bookResolvers.js";
-import userResolver from "./userResolvers.js";
-const resolvers = mergeResolvers([authorResolver, bookResolver, userResolver]);
-
-export default resolvers;
